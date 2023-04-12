9b2ee0cafd985ce68a589978cd401b0f4c8391c6
realchayka <lonelyplay54@gmail.com>
2023-04-12 22:06:06 +0300

    E-commerce on Node JS

---
+++ server/controllers/userController.js
@@ -0,0 +1,33 @@
+const ApiError = require('../error/ApiError')
+const bcrypt = require('bcrypt')
+const {User, Basket} = require('../models/models')
+const jwt = require('jsonwebtoken')
+class UserController {
+    async registration(req,res, next){
+        const {email, password, role} = req.body
+        if(!email || !password){
+            return next(ApiError.badRequest('Некорректный email или пароль'))
+        }
+        const candidate = await User.findOne({where:{email}})
+        if(candidate){
+            return next(ApiError.badRequest('Пользователь с таким email уже существует'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, role, password: hashPassword})
+        const basket = await Basket.create({userId: user.id})
+        const token = jwt.sign({id: user.id, email, role}, process.env.SECRET_KEY, {expiresIn: '24h'})
+        return res.json({token})
+    }
+    async login(req,res){
+
+    }
+    async check(req,res,next){
+        const {id} = req.query
+        if (!id){
+           return  next(ApiError.badRequest('Не задан ID'))
+        }
+        res.json(id)
+    }
+}
+
+module.exports = new UserController()
